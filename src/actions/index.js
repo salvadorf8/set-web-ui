f6d8f1eaade941bb02748dc434ab2de8f3b895af
salvadorf8 <aztecsalvador@gmail.com>
2019-11-25 00:08:25 -0600

    SF - made use of the baseURL like it was before, I'll revisit the proxy code to fix and use or I can just leave it like it was

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,9 @@
-// import herokuappEmail from '../api/herokuappEmail';
-import axios from 'axios';
+import herokuappEmail from '../api/herokuappEmail';
+// import axios from 'axios';
 
 export const sendEmail = (values) => async (dispatch) => {
     console.log('in action creator ', values);
-    const response = await axios.post('/email/sendform', values);
+    const response = await herokuappEmail.post('/email/sendform', values);
 
     dispatch({ type: 'SEND_EMAIL', payload: response });
 };
