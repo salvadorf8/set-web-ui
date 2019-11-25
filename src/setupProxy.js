b0812f6f702202a9fb2c1f471c482da82da7c91e
salvadorf8 <aztecsalvador@gmail.com>
2019-11-25 00:00:06 -0600

    SF - temporary commit - working on second bug where proxy is not working as expected

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,10 +1,12 @@
 const proxy = require('http-proxy-middleware');
 
 if (process.env.NODE_ENV === 'production') {
+    console.log(process.env.NODE_ENV);
     module.exports = function(app) {
         app.use(proxy(['/email'], { target: 'https://set-web-api.herokuapp.com' }));
     };
 } else {
+    console.log(process.env.NODE_ENV);
     module.exports = function(app) {
         app.use(proxy(['/email'], { target: 'http://localhost:3001' }));
     };
