8e01828c13ea2beffc2f36d7f37f880a51001427
salvadorf8 <aztecsalvador@gmail.com>
2020-06-24 17:47:02 -0500

    SF - updated api's new url name

--- src/api/herokuappEmail.js
+++ src/api/herokuappEmail.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 // Not used, implemented a proxy to automatically use localhost if in DEV, or Heroku when in PROD
 export default axios.create({
-    baseURL: 'https://set-web-api.herokuapp.com/email/'
+    baseURL: 'https://streamline-wiring-api.herokuapp.com/email/'
     // baseURL: 'http://localhost:3001/email/'
 });
